50da3e8b267644c64eade090143697adc1a64cdb
codewani <kondwanimwape@gmail.com>
2024-08-15 16:53:24 -0400

    exercises2.1-2.5

---
+++ part-2/exercises2.1-2.5/courses/src/components/Course.jsx
@@ -0,0 +1,49 @@
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+const Part = (props) => {
+  return (
+    <p>
+      {props.name} {props.exercises}
+    </p>
+  );
+};
+const Content = (props) => {
+  let parts = props.parts
+  return (
+      <div>
+        {parts.map(part =>
+          <div key={part.id}>
+            <Part key={part.id} name={part.name} exercises={part.exercises} />
+          </div>
+        )}
+      </div>
+  );
+};
+const Total = (props) => {
+  const {parts} = props
+  let exercises = parts.map(part => part.exercises);
+  let sum = exercises.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  }, 0);
+  return <p>Total {sum}</p>;
+};
+
+
+const Course = (props) => {
+  let courses = props.courses
+  console.log(courses)
+  return (
+    <div>
+      {courses.map(course =>
+        <div key={course.name}>
+        <Header course={course.name} />
+        <Content parts = {course.parts}/>
+        <Total parts = {course.parts}/>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Course;
