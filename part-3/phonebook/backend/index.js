c4a494ed000c5406c814fc66bd5f3f6f11918713
Codewani <kondwanimwape013@gmail.com>
2024-12-19 21:52:31 -0500

    exercises3.1-3.8

---
+++ part-3/phonebook/backend/index.js
@@ -0,0 +1,104 @@
+const express = require('express');
+const app = express();
+const logger = require('morgan');
+
+app.use(express.json());
+
+logger.token('body', (req, res) => {
+  if (req.method === "POST") {
+    return JSON.stringify(req.body)
+  }
+  else {
+    return ""
+  }
+})
+app.use(logger(':method :url :status :res[content-length] - :response-time ms :body'));
+let phonebook = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>');
+});
+  
+app.get('/api/phonebook', (request, response) => {
+    response.json(phonebook);
+});
+
+app.get('/api/phonebook/:id', (request, response) => {
+    const id = request.params.id
+    const contact = phonebook.find(contact => contact.id === id)
+
+    if (contact) {
+        response.json(contact);
+    }
+    else {
+        response.status(404).end();
+    }
+  });
+
+app.delete('/api/phonebook/:id', (request, response) => {
+    const id = request.params.id
+    phonebook = phonebook.filter(contact => contact.id !== id);
+  
+    response.status(204).end();
+})
+
+const generateId = () => {
+    const maxId = phonebook.length > 0
+      ? Math.max(...phonebook.map(n => Number(n.id)))
+      : 0
+    return String(maxId + 1);
+  }
+  
+app.post('/api/phonebook', (request, response) => {
+    console.log("This is a post request")
+    console.log(request.body)
+    const body = request.body
+    const duplicate = Boolean(phonebook.find(n => n.name === body.name))
+    
+    if (duplicate) {
+      return response.status(400).json({ 
+        error: 'Name is already in phonebook' 
+        });
+    }
+    if (!body.name || !body.number) {
+        return response.status(400).json({ 
+        error: 'The name or number is missing' 
+        });
+    }
+
+    const contact = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+
+    phonebook = phonebook.concat(contact)
+
+    response.json(phonebook);
+});
+
+const PORT = 3001
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
