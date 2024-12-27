78daab47108c27283bfb47db4e91e0745414c105
Codewani <kondwanimwape013@gmail.com>
2024-12-26 23:29:07 -0500

    Part-3 complete

--- part-3/phonebook/index.js
+++ part-3/phonebook/index.js
@@ -1,108 +1,119 @@
+const cors = require('cors')
 const express = require('express')
 const app = express()
 const logger = require('morgan')
-const cors = require('cors')
-
+const Contact = require('./models/phone')
+//const { notEqual } = require('assert')
 app.use(cors())
 
 app.use(express.json())
 app.use(express.static('dist'))
 
 logger.token('body', (req, res) => {
-  if (req.method === "POST") {
+  console.log(res)
+  if (req.method === 'POST') {
     return JSON.stringify(req.body)
   }
   else {
-    return ""
+    return ''
   }
 })
 app.use(logger(':method :url :status :res[content-length] - :response-time ms :body'))
-let phonebook = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+
 app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>');
-});
-  
-app.get('/api/phonebook', (request, response) => {
-    response.json(phonebook);
-});
-
-app.get('/api/phonebook/:id', (request, response) => {
-    const id = request.params.id
-    const contact = phonebook.find(contact => contact.id === id)
-
-    if (contact) {
-        response.json(contact);
-    }
-    else {
-        response.status(404).end();
-    }
-  });
-
-app.delete('/api/phonebook/:id', (request, response) => {
-    const id = request.params.id
-    phonebook = phonebook.filter(contact => contact.id !== id);
-  
-    response.status(204).end()
+  console.log('/')
+  response.send('<h1>Hello World!</h1>')
 })
 
-const generateId = () => {
-    const maxId = phonebook.length > 0
-      ? Math.max(...phonebook.map(n => Number(n.id)))
-      : 0
-    return String(maxId + 1)
+app.get('/api/phonebook', (request, response) => {
+  console.log('/api/phonebook')
+  Contact.find({}).then(contacts => {
+    response.json(contacts)
+  })
+})
+
+app.get('/api/phonebook/:id', (request, response) => {
+  const id = request.params.id
+  Contact.findById(id).then(contact => {
+    if (contact.length > 0) {
+      return response.json(contact)
+    }
+    else {
+      return response.status(404).end()
+    }
+  })
+})
+
+app.delete('/api/phonebook/:id', (request, response, next) => {
+  const id = request.params.id
+  Contact.findByIdAndDelete(id).then(result => {
+    console.log(result)
+    console.log('Contact was sucessfully deleted')
+  }).catch(error => {
+    next(error)
+  })
+  response.status(204).end()
+})
+
+// const generateId = () => {
+//   const maxId = phonebook.length > 0
+//     ? Math.max(...phonebook.map(n => Number(n.id)))
+//     : 0
+//   return String(maxId + 1)
+// }
+
+app.post('/api/phonebook', (request, response, next) => {
+  console.log('This is a post request')
+  console.log(request.body)
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'The name or number is missing'
+    })
   }
-  
-app.post('/api/phonebook', (request, response) => {
-    console.log("This is a post request")
-    console.log(request.body)
-    const body = request.body
-    const duplicate = Boolean(phonebook.find(n => n.name === body.name))
-    
-    if (duplicate) {
-      return response.status(400).json({ 
-        error: 'Name is already in phonebook' 
-        });
-    }
-    if (!body.name || !body.number) {
-        return response.status(400).json({ 
-        error: 'The name or number is missing' 
-        });
-    }
 
-    const contact = {
-        id: generateId(),
-        name: body.name,
-        number: body.number
-    }
+  const contact = new Contact({
+    name: body.name,
+    number: body.number
+  })
 
-    phonebook = phonebook.concat(contact)
+  contact.save().then(savedContact => {
+    response.json(savedContact)
+  }).catch(error => next(error))
+})
 
-    response.json(contact)
-});
+app.put('/api/phonebook/:id', (request, response, next) => {
+  const id = request.params.id
+  const { name, number } = request.body
+  Contact.findByIdAndUpdate(id, { name, number }, { new: true, runValidators: true, context: 'query' }).then(result => {
+    response.json(result)
+  }).catch(error => next(error))
+})
 
-const PORT = process.env.PORT || 3001
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  else if (error.name === 'ValidationError') {
+    return response.status(400).send({ error: error.message })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
